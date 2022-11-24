62fd436e936e4dd3b955376cd5234a9f5501fdab
George Reeve <george.fr.reeve@gmail.com>
2022-11-24 15:14:40 +0000

    Delete todo from database.

--- src/components/Reminder.js
+++ src/components/Reminder.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import {useTodosState} from "../store";
 
+function deleteTodo(id) {
+  fetch(`http://localhost:5001/toDo/${id}`, { method: "DELETE" });
+}
+
 export function Reminder({ id, title }) {
   const [todos, setTodos] = useTodosState();
   return (
@@ -10,6 +14,7 @@ export function Reminder({ id, title }) {
         const newTodos = todos.map(a => ({...a}));
         newTodos.splice(newTodos.indexOf(newTodos.find(todo => todo.id === id)), 1)
         setTodos(newTodos);
+        deleteTodo(id);
       }}>X</DeleteButton>
     </Section>
   )
